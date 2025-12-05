a3ae92f7a520437cb5668ac0f13d17bf6083d20c
asep sudrajat <asep.sudrajat@msiglife.co.id>
2025-12-05 15:50:42 +0700

    backup to localhost

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
-export const BASE_URL: string = 'https://unoutraged-lida-unsliced.ngrok-free.dev';
+// export const BASE_URL: string = 'https://unoutraged-lida-unsliced.ngrok-free.dev';
+
+
+export const BASE_URL: string = 'http://localhost/:8081';
